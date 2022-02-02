135f31c96908951d79b69793bdf3000868ef8c7a
monstermiles <miles.emmitt.moss@gmail.com>
2022-02-02 13:06:42 -0600

    started route for rendering login page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -7,9 +7,15 @@ const { Post, User, Comment } = require('../models');
 //     res.render('homepage')
 // });
 
+
+/////////////////////////renders all the posts on the homepage/////////////////////////
 router.get('/', async (req, res) => {
     try {
+        
+        //returns an array of model instance objects
         const postData = await Post.findAll({
+            
+            //for each post, return its associate username
             include: [
                 {
                     model: User,
@@ -17,19 +23,45 @@ router.get('/', async (req, res) => {
                 },
             ],
         });
-
+        
+        //map over the postData array and turn each one into a plain js object
         const posts = postData.map((post) => post.get({ plain: true }))
-        // console.log(posts)
+        
+        //render the homepage handlebars, with the posts. 
         res.render('homepage', {
             posts,
+            
             // logged_in: req.session.logged_in
-
         })
         console.log(posts)
     } catch (err) {
         res.status(500).json(err)
     }
 });
+///////////////////////////////////////////////////////////////////////////
+
+
+
+/////////////////////////render login page/////////////////////////////////
+
+router.get('/login', async (req, res) => {
+    try {
+    res.render('login')
+    } catch (err) {
+        res.json(err)
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
 
 
 module.exports = router;
